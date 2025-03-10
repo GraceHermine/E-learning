2abd971f17f094d326bcfa6a7aeba94ff81da5e8
GraceHermine <dedjenehermine@gmail.com>
2025-03-10 13:59:10 +0000

    ajout de classe user dans models

--- IIT/views.py
+++ IIT/views.py
@@ -1,3 +1,19 @@
+from django.db import models
 from django.shortcuts import render
 
 # Create your views here.
+class Utilisateur(models.Model):
+
+    class Meta:
+        verbose_name = "Utilisateur"
+        verbose_name_plural = "Utilisateurs"
+
+    nom = models.CharField(max_length=255)
+    prenom = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    telephone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)  
+
+    def _str_(self):
+        return f"{self.nom} {self.prenom}"
