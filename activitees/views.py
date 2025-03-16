949eeee448092cdc016cf8a9587469eb87ebc2c2
Conquerant2K06 <162891027+Conquerant2K06@users.noreply.github.com>
2025-03-16 23:57:07 +0000

    poussage

--- activitees/views.py
+++ activitees/views.py
@@ -1,19 +1,19 @@
 # activitees/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Departement, Club, Evenement
 
 def departement_list(request):
     departements = Departement.objects.all()
-    return render(request, 'Departements.html', {'departements': departements})
+    return render(request, 'activite.html', {'departements': departements})
 
 def club_list(request):
     clubs = Club.objects.all()
-    return render(request, 'clubs.html', {'clubs': clubs})
+    return render(request, 'activite.html', {'clubs': clubs})
 
 def evenement_list(request):
     evenements = Evenement.objects.all()
-    return render(request, 'evenements.html', {'evenements': evenements})
+    return render(request, 'activite.html', {'evenements': evenements})
 
 def activite_list(request):
     departements = Departement.objects.all()
@@ -24,3 +24,15 @@ def activite_list(request):
         'clubs': clubs,
         'evenements': evenements
     })
+
+def departement_detail(request, departement_id):
+    departement = get_object_or_404(Departement, id=departement_id)
+    return render(request, 'departement_detail.html', {'departement': departement})
+
+def club_detail(request, club_id):
+    club = get_object_or_404(Club, id=club_id)
+    return render(request, 'club_detail.html', {'club': club})
+
+def evenement_detail(request, evenement_id):
+    evenement = get_object_or_404(Evenement, id=evenement_id)
+    return render(request, 'evenement_detail.html', {'evenement': evenement})
